6fd67bb5d3ce52939258033e429b462415de592d
h2002y <h2002y@naver.com>
2023-04-26 23:46:33 +0900

    Add Props

--- movie-4-props/src/App.js
+++ movie-4-props/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const Button = (props) => (
+    <button style={{
+      width: props.width,
+      height: "50px",
+      border: "none",
+      borderRadius: "25px",
+      backgroundColor: props.bgColor,
+    }}>
+      {props.name}
+    </button>
   );
-}
+
+  // const Div = () => (
+  //   <>
+  //     <h4>props는 props 변수를 사용하는 대신</h4>
+  //     <h4>컴포넌트 파라미터 () 안에</h4>
+  //     <h4>중괄호 {'{'}속성명{'}'} 으로 속성명을 그대로 사용할 수 있다. </h4>
+  //   </>
+  // );
+
+  const Direct = ({fruitName}) => (
+    <h5>I am {fruitName}!</h5>
+  );
+
+  return (
+    <>
+      <Button bgColor="salmon" width="200px" name="Salmon Button" />
+      <Button bgColor="beige" width="300px" name="Beige Button" />
+      <Direct fruitName="Peach" />      
+      <Direct fruitName="Apple" />      
+      <Direct fruitName="Mango" />      
+      <Direct fruitName="Melon" />      
+    </>
+  );
+};
 
 export default App;
